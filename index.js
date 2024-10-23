f7d9f9900b518e559f3ea5f4659192b0e645024a
KarimAmador <karimalejandroAC@gmail.com>
2024-10-23 10:53:35 -0600

    feat: add error handling for /query/:search endpoint

--- index.js
+++ index.js
@@ -20,18 +20,23 @@ app.get('/', (req, res) => {
 app.get('/query/:search', async (req, res) => {
     console.log(req.params);
     console.log(req.query);
-
-    const results = await fetch(`${cse}?cx=${process.env.CX}&q=${encodeURI(req.params.search)}&start=${(Number(req.query.page) * 10) + 1}&searchType=image&key=${process.env.API_KEY}`).then(res => res.json());
-
-    if (results.searchInformation.totalResults == 0) {
-        return res.type('text').send(`No results for '${req.params.search}'`);
-    }
     
-    if (results.error) {
-        return res.status(400).json({ code: '400', message: `Page ${req.query.page} does not exist.` });
-    }
+    try {
+        const results = await fetch(`${cse}?cx=${process.env.CX}&q=${encodeURI(req.params.search)}&start=${(Number(req.query.page) * 10) + 1}&searchType=image&key=${process.env.API_KEY}`)
+                            .then(res => res.json());
 
-    res.json(results.items);
+        if (results.searchInformation?.totalResults == 0) {
+            return res.type('text').send(`No results for '${req.params.search}'`);
+        }
+    
+        if (results.error) {
+            return res.status(400).json(results.error);
+        }
+        
+        res.json(results.items);
+    } catch (err) {
+        return res.json(err);
+    }
 });
 
 app.use(function(req, res, next) {
