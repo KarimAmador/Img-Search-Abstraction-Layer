cfc3fd2d0a0d098a98e1d00d993dd59cc288eae1
KarimAmador <karimalejandroAC@gmail.com>
2024-10-24 11:37:46 -0600

    refactor: change button variable

--- public/script.js
+++ public/script.js
@@ -1,25 +1,25 @@
 const query = document.getElementById('query');
 const page = document.getElementById('page');
 const searchQuery = document.getElementById('searchQuery');
-const buttonClass = document.querySelector('.button').classList;
+const button = document.querySelector('.button');
 const base = window.location.href;
 
 searchQuery.innerText = base;
 
 const updateURL = (e) => {
     if (!query.value) {
-        buttonClass.add('disabled');
+        button.classList.add('disabled');
         searchQuery.innerText = base;
         searchQuery.setAttribute('href', base);
-        document.querySelector('.button').setAttribute('href', base);
+        button.setAttribute('href', base);
         return;
     };
     
     const search = `${base}query/${query.value}?page=${page.value}`;
     searchQuery.innerText = search;
     searchQuery.setAttribute('href', search);
-    document.querySelector('.button').setAttribute('href', search);
-    buttonClass.remove('disabled');
+    button.setAttribute('href', search);
+    button.classList.remove('disabled');
 }
 
 [query, page].forEach(i => i.addEventListener('change', updateURL));
